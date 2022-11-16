35028b60d7975a0931f4a5e486a7af100347fab7
meap02-Lenovo <79549780+meap02@users.noreply.github.com>
2022-11-15 17:10:03 -0800

    new mysql setup progress

--- sql.py
+++ sql.py
@@ -1,1 +1,11 @@
+import mysql
+import json
 
+
+cnx = mysql.connector.connect(user="batman",
+                              password="{your_password}",
+                              host="lph.mysql.database.azure.com",
+                              port=3306,
+                              database="{your_database}",
+                              ssl_ca="{ca-cert filename}",
+                              ssl_disabled=False)
