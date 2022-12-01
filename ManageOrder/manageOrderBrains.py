f7ea227276e8705cb1059c776f09378de99df01f
20ringtaft <ringtaft@gmail.com>
2022-11-30 22:00:04 -0800

    progress

--- ManageOrder/manageOrderBrains.py
+++ ManageOrder/manageOrderBrains.py
@@ -1,10 +1,26 @@
 from manageOrder import *
 import sys
+from PyQt5 import *
+
 
 class manageOrder(Ui_MainWindow):
     def __init__(self, window):
         self.setupUi(window)
+        self.backButton.clicked.connect(self.clickBack)
+        self.manageButton.clicked.connect(self.clickManage)
+        
 
+    def clickBack(self):
+        print("back test")
+
+    def clickManage(self):
+        print("Manage test")
+
+    def clickList(self):
+        print("Test List")
+        
+    def getOrder(self):
+       self.ordersList.clicked.connect(self.clickList)
 
 app = QtWidgets.QApplication(sys.argv)
 MainWindow = QtWidgets.QMainWindow()
