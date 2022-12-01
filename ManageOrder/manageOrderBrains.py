42ad573d9b13963adacdcfeeda440e9af3ed5201
20ringtaft <ringtaft@gmail.com>
2022-11-30 22:48:08 -0800

    progress

--- ManageOrder/manageOrderBrains.py
+++ ManageOrder/manageOrderBrains.py
@@ -1,18 +1,21 @@
 from manageOrder import *
 import sys
+from PyQt5 import *
 
 
 
 class manageOrder(Ui_MainWindow):
     def __init__(self, window):
+        #create ui
         self.setupUi(window)
+        #button clickability
         self.backButton.clicked.connect(self.clickBack)
         self.manageButton.clicked.connect(self.clickManage)
         
         order1 = 'Order 1'
         order2 = "order 2"
 
-        
+        #fills the list with dummy data
         self.ordersList.addItem(order1)
         self.ordersList.addItem(order2)
         self.ordersList.addItem(order1)
@@ -28,7 +31,7 @@ class manageOrder(Ui_MainWindow):
         self.ordersList.addItem(order1)
         self.ordersList.addItem(order2)
 
-
+    #this literally crashes the program but it closes it :)
     def clickBack(self):
         self.close()
 
@@ -38,7 +41,7 @@ class manageOrder(Ui_MainWindow):
     def clickList(self):
         print("Test List")
 
-
+#creates the window
 app = QtWidgets.QApplication(sys.argv)
 MainWindow = QtWidgets.QMainWindow()
 ui = manageOrder(MainWindow)
