cabafc60d79fcd05ace59ab7f27a6b5a81268a22
russellric <116527694+russellric@users.noreply.github.com>
2022-11-30 22:23:25 -0800

    changing cookAttached methods to read arrays

--- Cook/cookAttached.py
+++ Cook/cookAttached.py
@@ -1,90 +1,83 @@
 from cookGUI import *
 import sys
 
-class cookGUIAttached(Ui_cookGUI):
-    #constructor
+class cookAttached(Ui_cookGUI):
+    #making constructor
     def __init__(self, window):
         self.setupUi(window)
 
-        self.doneButton1.clicked.connect(self.updateOrderSlot1)
-        self.doneButton2.clicked.connect(self.updateOrderSlot2)
-        self.doneButton3.clicked.connect(self.updateOrderSlot3)
-        self.doneButton4.clicked.connect(self.updateOrderSlot4)
+        #set up buttons to reset each slot everytime it is presses
+        self.doneButton1.clicked.connect(self.updateOrderSlot1)     #update slot 1
+        self.doneButton2.clicked.connect(self.updateOrderSlot2)     #update slot 2
+        self.doneButton3.clicked.connect(self.updateOrderSlot3)     #update slot 3
+        self.doneButton4.clicked.connect(self.updateOrderSlot4)     #update slot 4
 
+    #edit slot 1
     def updateOrderSlot1(self):
-        test1 = 1
-        test2 = "Walter's Nuggies"
-        test3 = 14
-        test4 = "medium rare"
+        #initializing test array to pull data from
+        orderArray = [1,"walter's Nuggies",14,"medium rare"]
 
         orderlength = 3
         orderStr = ""
         for item in range(orderlength):
-                quantity = str(test3)
-                name = str(test2)
-                note = str(test4)
+                quantity = str(orderArray[2])
+                name = str(orderArray[1])
+                note = str(orderArray[3])
                 orderStr += (quantity + "  " + name + "\n" + note + "\n\n")
 
-        self.orderNum1.setText(str(test1))
+        self.orderNum1.setText(str(orderArray[0]))
         self.textArea1.setText(orderStr)
-        print(orderStr)
 
+    #edit slot 2
     def updateOrderSlot2(self):
-        test1 = 2
-        test2 = "glizzy"
-        test3 = 420
-        test4 = "extra small"
+        #initializing test array to pull data from
+        orderArray = [1,"walter's Nuggies",14,"medium rare"]
 
         orderlength = 3
         orderStr = ""
         for item in range(orderlength):
-                quantity = str(test3)
-                name = str(test2)
-                note = str(test4)
+                quantity = str(orderArray[2])
+                name = str(orderArray[1])
+                note = str(orderArray[3])
                 orderStr += (quantity + "  " + name + "\n" + note + "\n\n")
 
-        self.orderNum2.setText(str(test1))
+        self.orderNum2.setText(str(orderArray[0]))
         self.textArea2.setText(orderStr)
-        print(orderStr)
 
+    #edit slot 3
     def updateOrderSlot3(self):
-        test1 = 3
-        test2 = "dirt water"
-        test3 = 2
-        test4 = "extra dirty"
+        #initializing test array to pull data from
+        orderArray = [1,"walter's Nuggies",14,"medium rare"]
 
         orderlength = 3
         orderStr = ""
         for item in range(orderlength):
-                quantity = str(test3)
-                name = str(test2)
-                note = str(test4)
+                quantity = str(orderArray[2])
+                name = str(orderArray[1])
+                note = str(orderArray[3])
                 orderStr += (quantity + "  " + name + "\n" + note + "\n\n")
 
-        self.orderNum3.setText(str(test1))
+        self.orderNum3.setText(str(orderArray[0]))
         self.textArea3.setText(orderStr)
-        print(orderStr)
 
+    #edit slot 4
     def updateOrderSlot4(self):
-        test1 = 4
-        test2 = "poop"
-        test3 = 14
-        test4 = "ur mom"
+        #initializing test array to pull data from
+        orderArray = [1,"walter's Nuggies",14,"medium rare"]
 
         orderlength = 3
         orderStr = ""
         for item in range(orderlength):
-                quantity = str(test3)
-                name = str(test2)
-                note = str(test4)
+                quantity = str(orderArray[2])
+                name = str(orderArray[1])
+                note = str(orderArray[3])
                 orderStr += (quantity + "  " + name + "\n" + note + "\n\n")
 
-        self.orderNum4.setText(str(test1))
+        self.orderNum4.setText(str(orderArray[0]))
         self.textArea4.setText(orderStr)
-        print(orderStr)
 
 app = QtWidgets.QApplication(sys.argv)
 MainWindow = QtWidgets.QMainWindow()
-ui = cookGUIAttached(MainWindow)
+ui = cookAttached(MainWindow)
 MainWindow.show()
 app.exec_()
