b1f6ce7fe26847b64eead41117930f337bb68d36
Abdul Kalam Syed <abdulksyed10@gmail.com>
2022-12-01 22:52:00 -0800

    changing the location of buttonsFunctions.

---
+++ buttonsFunctions.py
@@ -0,0 +1,30 @@
+import sys
+from PyQt5 import *
+from PyQt5.QtWidgets import QMainWindow
+from PyQt5.QtCore import Qt
+
+from welcomeScreen import *
+from selectingItems import *
+from finishOrder import *
+
+class buttonsFunctions(Ui_welcomeScreen):
+
+    def __init__(self, window):
+        self.setupUi(window)
+
+        self.createOrderBtn.clicked.connect(self.switchToSelectingItems)
+
+    def switchToSelectingItems(self):
+        self.selectingItemsWindow = QtWidgets.QMainWindow()
+        self.ui = Ui_selectingItems()
+        self.ui.setupUi(self.selectingItemsWindow)
+        self.selectingItemsWindow.show()
+
+
+
+app = QtWidgets.QApplication(sys.argv)
+
+WelcomeScreenWindow = QtWidgets.QMainWindow()
+ui = buttonsFunctions(WelcomeScreenWindow)
+WelcomeScreenWindow.show()
+app.exec_()
