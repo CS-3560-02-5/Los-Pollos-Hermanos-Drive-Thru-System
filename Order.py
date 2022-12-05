40240fa88c51cfe4dc62a404f0f44d468284ed72
20ringtaft <ringtaft@gmail.com>
2022-12-05 01:22:32 -0800

    partial fix

--- Order.py
+++ Order.py
@@ -11,7 +11,8 @@ import sys
 class Order:
     queue = cycle(range(1, 200))
 
-    def __init__(self, customer_name: str, *, order_id=uuid.uuid1().hex, order_status='s', queue_num=-1):
+    def __init__(self, customer_name: str, *, order_id="", order_status='s', queue_num=-1):
+        self.order_id = uuid.uuid1().hex
         self.customer_name = customer_name
         self.order_id = order_id
         self.queue_num = next(self.queue)
