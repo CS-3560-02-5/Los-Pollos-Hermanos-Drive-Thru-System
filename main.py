65450236e17b590fcc16b2f7f7699b7c34b92f1e
russellric <116527694+russellric@users.noreply.github.com>
2022-12-04 20:38:50 -0800

    Update main.py

--- main.py
+++ main.py
@@ -8,12 +8,17 @@ sys.path.append("Manager")
 sys.path.append("Cook")
 sys.path.append("Cashier")
 import manageOrderAttached
-import managerGUI
-import managerAttatched
+import cookAttached
+import cookGUI
 
 
 
 ####### Initial setup of running environment
 
+mass = data_bridge.bridge("sql.json")
 
+app = QtWidgets.QApplication(sys.argv)
+cookGUIWindow = QtWidgets.QMainWindow()
+CookWindowUI = cookAttached.cookAttached(mass, cookGUIWindow)
+app.exec()
 
