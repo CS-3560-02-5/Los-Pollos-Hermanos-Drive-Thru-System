4d9ffefaa4d8452240e41c1bd443a95b581ad216
meap02 <79549780+meap02@users.noreply.github.com>
2022-12-05 12:51:10 -0800

    Manager UI fully implemented
    I had to fix the data_bridge in a couple of places as welk

--- main.py
+++ main.py
@@ -7,16 +7,15 @@ from PyQt5 import QtCore, QtWidgets
 sys.path.append("Manager")
 sys.path.append("Cook")
 sys.path.append("Cashier")
-import manageOrderAttached
-import managerGUI
 import managerAttached
 
 
 
 ####### Initial setup of running environment
-mass = data_bridge.bridge()
+mass = data_bridge.bridge("sql.json")
 
 app = QtWidgets.QApplication(sys.argv)
 manager_win = QtWidgets.QMainWindow()
 manager_ui = managerAttached.managerAttached(mass, manager_win)
+app.exec_()
 
